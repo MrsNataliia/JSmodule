15e193fc97129428f82652be779842c9f96204e0
MrsNataliia <natalyanaumenko76@gmail.com>
2023-08-16 15:42:31 +0300

    add Task1.1.html and Task1-1.js

---
+++ Lesson1/Task1-1.js
@@ -0,0 +1,49 @@
+//task1
+
+let s1="Hello";
+console.log(s1);
+let s2="owu";
+console.log(s2);
+let s3="com";
+console.log(s3);
+let s4="ua";
+console.log(s4);
+let n1=1;
+console.log(n1);
+let n2=10;
+console.log(n2);
+let n3=-999;
+console.log(n3);
+let n4=123;
+console.log(n4);
+let n5=3.14;
+console.log(n5);
+let n6=2.16;
+console.log(n6);
+let n7=16;
+console.log(n7);
+let b1=false;
+console.log(b1);
+let b2=true;
+console.log(b2);
+
+
+//task2
+
+let firstName = "Nataliia";
+let midlName = "Volodymyrivna";
+let lastName ="Naumenko";
+let result = firstName + " " + midlName + " " + lastName;
+console.log(result)
+
+
+//task3
+
+let a = 100;
+let b="100";
+let c =true;
+console.log (typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+
